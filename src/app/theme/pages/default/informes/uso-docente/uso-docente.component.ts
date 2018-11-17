072a8933aafc2fb2f0bff2a66d3570802e76ec4e
stivenbr <stivenbr12@hotmail.com>
2018-11-17 11:09:48 -0500

    Cambios Adminsitradpr

--- src/app/theme/pages/default/informes/uso-docente/uso-docente.component.ts
+++ src/app/theme/pages/default/informes/uso-docente/uso-docente.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, AfterViewInit } from '@angular/core';
+import { Helpers } from '../../../../../helpers';
+import { ScriptLoaderService } from '../../../../../_services/script-loader.service';
 
+declare let google: any;
+declare let GoogleChartsDemo: any;
 @Component({
     selector: 'app-uso-docente',
     templateUrl: './uso-docente.component.html',
     encapsulation: ViewEncapsulation.None,
 })
-export class UsoDocenteComponent implements OnInit {
+export class UsoDocenteComponent implements OnInit, AfterViewInit {
 
-    constructor() {
+    constructor(private _script: ScriptLoaderService) {
+
     }
-
     ngOnInit() {
+
     }
+    ngAfterViewInit() {
+        this._script.loadScripts('app-charts-google-charts',
+            ['assets/demo/default/custom/components/charts/google-charts.js']);
+        google.load('visualization', '1', { packages: ['corechart', 'bar', 'line'], callback: GoogleChartsDemo.runDemos() });
+    }
+
 }
